167cc0813a4e3b833b748842691cf95115acf254
Kshitij Kumar <119648768+kshitij-y@users.noreply.github.com>
2024-08-26 16:03:30 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://kshitij:esdXfLs$ywF42a.@cluster0.ampvoxv.mongodb.net/simple-todo-app");
+mongoose.connect("mongodb+srv://kshitij:**********@cluster0.ampvoxv.mongodb.net/simple-todo-app");
 
 const todoSchema = mongoose.Schema({
     title: String,
